a1b19dda9979cf3dd6291bf5c90d70d7ca15d246
ericzhuo <eztlvit@gmail.com>
2016-03-24 15:38:11 +0800

    first commit

---
+++ tc/src/main/java/tc/com/Print.java
@@ -0,0 +1,69 @@
+package tc.com;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import tc.com.entity.User;
+
+@Controller
+public class Print {
+
+	private String msg;
+	
+	private DispatcherServlet dispatcherServlet;
+
+	private InternalResourceViewResolver internalResourceViewResolver;
+
+	@RequestMapping(value = "/print")
+	public String print(Model model) {
+		this.msg = "hello --";
+		System.out.println("-------------");
+		model.addAttribute("msg", msg);
+		return "index";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/initReprot")
+	public String initHighcharts(
+			@RequestParam(value = "char", required = false) String c,
+			User u)
+			throws JSONException {
+		System.out.println(c);
+		System.out.println(u.getPhone());
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject = new JSONObject();
+		JSONArray tempJsonArray = new JSONArray();
+		tempJsonArray.put(5);
+		tempJsonArray.put(7);
+		tempJsonArray.put(3);
+		jsonObject.put("name", "t11111");
+		jsonObject.put("data", tempJsonArray);
+		jsonArray.put(jsonObject);
+		tempJsonArray = new JSONArray();
+		tempJsonArray.put(1);
+		tempJsonArray.put(0);
+		tempJsonArray.put(4);
+		tempJsonArray.put(9);
+		tempJsonArray.put(16);
+		JSONObject jsonObject1 = new JSONObject();
+		jsonObject1.put("name", "t22222");
+		jsonObject1.put("data", tempJsonArray);
+		jsonArray.put(jsonObject1);
+		System.out.println(jsonArray.toString());
+		return jsonArray.toString();
+	}
+
+	@RequestMapping(value = "/submit")
+	public String submitForm(User user) {
+		System.out.println(user.getName());
+		return "index";
+	}
+}
---
+++ tc/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript"
+	src="http://cdn.hcharts.cn/jquery/jquery-1.8.3.min.js"></script>
+<script type="text/javascript"
+	src="http://cdn.hcharts.cn/highcharts/highcharts.js"></script>
+<script type="text/javascript"
+	src="http://cdn.hcharts.cn/highstock/highstock.js"></script>
+<script type="text/javascript"
+	src="http://cdn.hcharts.cn/highmaps/highmaps.js"></script>
+<script type="text/javascript">
+	function initReport() {
+		var user = {
+			name : "e1",
+			phone : "11张机票购买未满",
+			address : "t1"
+		};
+		$.ajax({
+			type : "post",
+			dataType : "json",
+			data : user,
+			url : "/tc/initReprot",
+			success : function(data) {
+				initChart(data);
+			}
+		});
+	};
+	
+	function initChart(data){
+		$('#container').highcharts({
+			char : {
+				type : 'column'
+			},
+			title : {
+				text : 'My first Highcharts chart'
+			},
+			xAxis : {
+				categories : [ 'my', 'first', 'chart' ]
+			},
+			yAxis : {
+				title : {
+					text : 'something'
+				}
+			},
+			series : data
+		});
+	};
+
+	$(function() {
+		initReport();
+	});
+	
+</script>
+<title>Insert title here</title>
+</head>
+<body>
+	<div id="container" style="min-width: 700px; height: 400px"></div>
+	${msg}
+	<form action="/tc/submit" method="post" enctype="application/x-www-form-urlencoded">
+		<input name="name" value="张" type="text" /> 
+		<input name="phone" value="三" type="text" />
+		<input name="address" value="三" type="text" />
+		<input type="submit" name="submit" value="submit" />
+	</form>
+</body>
+</html>
